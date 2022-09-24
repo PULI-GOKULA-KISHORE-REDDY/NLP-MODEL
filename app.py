ae2a25f86a5fca9a727de83bc8586564f912c061
PULI GOKULA KISHORE REDDY <94065306+PULI-GOKULA-KISHORE-REDDY@users.noreply.github.com>
2022-09-24 22:56:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,url_for,request
 import pandas as pd 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.externals import joblib
+#from sklearn.externals import joblib
 
 
 app = Flask(__name__)
